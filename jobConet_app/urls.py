8f7ddd3df4f5aa95a10f74db27a46a9696835fda
Luisa <lu.andra.808@gmail.com>
2024-11-25 19:28:03 -0500

    realice el cambio en la busqueda para empleo y bsuqueda normal

--- jobConet_app/urls.py
+++ jobConet_app/urls.py
@@ -3,11 +3,12 @@ from . import views  # Importar las vistas
 
 urlpatterns = [
     path('', views.home, name='home'),  # Ruta principal
-    path('agregar/', views.agregar_publicacion, name='agregar_publicacion'),  # Vista para agregar publicaciones
+    path('agregar/', views.agregar_publicacion, name='agregar_publicacion'),
+    path('buscar/', views.oportunidades, name='oportunidades'),# Vista para agregar publicaciones
     path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('buscar-empleo/', views.buscar_empleo, name='buscar_empleo'),
     path('update_post/<int:post_id>/', views.update_post, name='update_post'),
     path('registrar/', views.registrar_usuario, name='registrar_usuario'),  # Vista para registrar un nuevo usuario
     path('', views.lista_publicaciones, name='lista_publicaciones'),  # Vista para listar publicaciones
     path('perfil/', views.perfil_usuario, name='perfil_usuario'),
 ]
-
